fc92cbed4baccdeaa6ef2fade926a0ee5ca50cdf
eddyiiz <eddyxsayf@gmail.com>
2023-11-28 06:03:08 +0200

    Add project files.

---
+++ Ecommerce/Server/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿namespace Ecommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 2,
+                    Title = "Ready Player One",
+                    Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
+                    Price = 7.99m
+                }
+                );
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce/Server/Migrations/20231128034436_ProductSeeding.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Ready Player One is a 2011 science fiction novel, and the debut novel of American author", "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg", 7.99m, "Ready Player One" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
